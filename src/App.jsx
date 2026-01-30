63dbc0c8ee969048b4e9d6923b9c960376ffbde2
koreahghg <koreahghg@gmail.com>
2026-01-30 17:40:27 +0900

    주사위 숫자 변경 버튼

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
-import Dice from './Dice';
+import { useState } from "react";
+import Button from "./Button";
+import Dice from "./Dice";
+
+function random(n) {
+    return Math.ceil(Math.random() * n);
+}
 
 function App() {
-    return (
-        <div>
-            <h1>주사위 게임</h1>
-            <Dice color="red"/>
-        </div>
-    );
+  const [num, setNum] = useState(1);
+
+  const handleClick1 = () => {
+    const nextNum = random(6);
+    setNum(nextNum);
+  }
+
+  const handleClick2 = () => {
+    setNum(1);
+  }
+
+  return (
+    <div>
+      <h1>주사위 게임</h1>
+      <div>
+        <Button onClick={handleClick1}>던지기</Button>
+        <Button onClick={handleClick2}>처음부터</Button>
+      </div>
+      <Dice color="red" num={num} />
+    </div>
+  );
 }
 
 export default App;
